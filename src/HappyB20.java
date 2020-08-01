d017c61270b88487e2a400c4ae71034ef2f57314
neciporhan <68977036+neciporhan@users.noreply.github.com>
2020-08-01 14:36:04 -0600

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,7 +3,7 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
         System.out.println("B20 is the best");
-
+//I made a change
     }
 }
 /* git init
