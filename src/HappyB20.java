d93b2c0cf3da984bec855df5d07923b0a3f551b1
Necip Orhan <neciporhan@Necips-MacBook-Air.local>
2020-08-01 10:34:36 -0600

    add HappyAgain.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,6 +2,12 @@ public class HappyB20 {
 
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+        System.out.println("B20 is the best");
 
     }
 }
+/* git init
+   git add .
+   -m"add HappyB20.java"
+   -m"update HappyB20.java"
+*/
